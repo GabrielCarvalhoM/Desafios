fd05aa93c2ac5ac2bb77b7e947034165ec381579
GabrielCarvalhoM <c.gabrielmaciel@icloud.com>
2022-10-03 22:03:12 -0300

    primeiro commit

---
+++ Desafio/Sources/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Desafio
+//
+//  Created by Gabriel Carvalho on 03/10/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = HomeViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+
+}
+
---
+++ Desafio/Sources/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Desafio
+//
+//  Created by Gabriel Carvalho on 03/10/22.
+//
+
+import UIKit
+
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .green
+        
+    }
+}
